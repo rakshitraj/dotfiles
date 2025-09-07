d084084ef18a9854fcb4ddf8489b0fd60ecb18d4
Rakshit Raj <rakshit3181@gmail.com>
2025-09-07 21:41:44 +0530

    set virtualenvs.in-project true

--- dotfiles/install_poetry.sh
+++ dotfiles/install_poetry.sh
@@ -3,3 +3,7 @@
 echo "📦 Installing python-poetry..."
 curl -sSL https://install.python-poetry.org | python3 -
 poetry --version
+
+echo "Enable poetry to create virtualenvs in project...
+poetry config virtaulenvs.in-project true
+echo "✅ Done."
